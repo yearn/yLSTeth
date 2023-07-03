1a3797996c2b524a27be20851151f872e7a16608
Major <major@ycorpo.com>
2023-07-03 13:51:49 +0200

    fix: scroll

--- components/views/Phase1.tsx
+++ components/views/Phase1.tsx
@@ -21,94 +21,97 @@ function Phase1({variant}: {variant: string[]}): ReactElement {
 	const {whitelistStatus} = periods || {};
 
 	return (
-		<section className={'absolute inset-x-0 grid grid-cols-12 gap-0 px-4 pt-10 md:gap-20 md:pt-12'}>
-			<div className={'col-span-12 md:col-span-6 md:mb-0'}>
-				<div className={'mb-10 flex flex-col justify-center'}>
-					<motion.p
-						className={'text-lg'}
-						variants={customVariants(0)}
+		<section className={'absolute inset-x-0'}>
+			<div
+				className={'scrollfix grid grid-cols-12 gap-0 px-4 pt-10 md:gap-20 md:pt-12'}>
+				<div className={'col-span-12 h-full md:col-span-6 md:mb-0'}>
+					<div className={'mb-10 flex flex-col justify-center'}>
+						<motion.p
+							className={'text-lg'}
+							variants={customVariants(0)}
+							custom={variant}
+							initial={'initial'}
+							animate={'move'}
+							exit={'exit'}>
+							{'Current Phase'}
+						</motion.p>
+						<motion.h1
+							className={'text-3xl md:text-8xl'}
+							variants={customVariants(0.02)}
+							custom={variant}
+							initial={'initial'}
+							animate={'move'}
+							exit={'exit'}>
+							{'Whitelisting'}
+						</motion.h1>
+						<motion.b
+							suppressHydrationWarning
+							className={'font-number mt-4 text-3xl text-purple-300 md:text-4xl'}
+							variants={customVariants(0.04)}
+							custom={variant}
+							initial={'initial'}
+							animate={'move'}
+							exit={'exit'}>
+							<Timer />
+						</motion.b>
+					</div>
+
+					<motion.div
+						className={'mb-8 text-neutral-700'}
+						variants={customVariants(0.05)}
 						custom={variant}
 						initial={'initial'}
 						animate={'move'}
 						exit={'exit'}>
-						{'Current Phase'}
-					</motion.p>
-					<motion.h1
-						className={'text-3xl md:text-8xl'}
-						variants={customVariants(0.02)}
+						<p>{'Want your LST to be included in yETH’s basket of tokens? You’ve come to the right place. Get whitelisted for your LST to take part in yETH Bootstrapping.'}</p>
+					&nbsp;
+						<ul className={'list-outside list-disc pl-4'}>
+							<li className={'font-bold'}>
+								{'Pay a non refundable 1 ETH fee (to prevent spam).'}
+							</li>
+							<li className={'font-bold'}>
+								{'Fill in the form.'}
+							</li>
+							<li className={'font-bold'}>
+								{'Wait for the Yearn rug detection unit to check your application.'}
+							</li>
+						</ul>
+					&nbsp;
+						<p>{'Applications are checked for obvious scams, but nothing further. Genuine applications will be Whitelisted to take part in the super fun Bootstrapping Incentive Games. Yay.'}</p>
+					</motion.div>
+
+					<motion.div
+						variants={customVariants(0.06)}
 						custom={variant}
 						initial={'initial'}
 						animate={'move'}
 						exit={'exit'}>
-						{'Whitelisting'}
-					</motion.h1>
-					<motion.b
-						suppressHydrationWarning
-						className={'font-number mt-4 text-3xl text-purple-300 md:text-4xl'}
-						variants={customVariants(0.04)}
-						custom={variant}
-						initial={'initial'}
-						animate={'move'}
-						exit={'exit'}>
-						<Timer />
-					</motion.b>
+						<Link
+							href={whitelistStatus === 'ended' ? '/' : '/form'}
+							target={'_blank'}
+							rel={'noopener noreferrer'}>
+							<Button
+								isDisabled={whitelistStatus === 'ended'}
+								suppressHydrationWarning
+								className={'yearn--button w-full rounded-md !text-sm md:w-1/2'}>
+								{whitelistStatus === 'ended' ? 'Check whitelisted projects' : 'Take me to the form'}
+							</Button>
+						</Link>
+					</motion.div>
 				</div>
 
 				<motion.div
-					className={'mb-8 text-neutral-700'}
-					variants={customVariants(0.05)}
-					custom={variant}
-					initial={'initial'}
-					animate={'move'}
-					exit={'exit'}>
-					<p>{'Want your LST to be included in yETH’s basket of tokens? You’ve come to the right place. Get whitelisted for your LST to take part in yETH Bootstrapping.'}</p>
-					&nbsp;
-					<ul className={'list-outside list-disc pl-4'}>
-						<li className={'font-bold'}>
-							{'Pay a non refundable 1 ETH fee (to prevent spam).'}
-						</li>
-						<li className={'font-bold'}>
-							{'Fill in the form.'}
-						</li>
-						<li className={'font-bold'}>
-							{'Wait for the Yearn rug detection unit to check your application.'}
-						</li>
-					</ul>
-					&nbsp;
-					<p>{'Applications are checked for obvious scams, but nothing further. Genuine applications will be Whitelisted to take part in the super fun Bootstrapping Incentive Games. Yay.'}</p>
-				</motion.div>
-
-				<motion.div
+					className={'relative col-span-12 hidden h-[100%] md:col-span-6 md:flex'}
 					variants={customVariants(0.06)}
 					custom={variant}
 					initial={'initial'}
 					animate={'move'}
 					exit={'exit'}>
-					<Link
-						href={whitelistStatus === 'ended' ? '/' : '/form'}
-						target={'_blank'}
-						rel={'noopener noreferrer'}>
-						<Button
-							isDisabled={whitelistStatus === 'ended'}
-							suppressHydrationWarning
-							className={'yearn--button w-full rounded-md !text-sm md:w-1/2'}>
-							{whitelistStatus === 'ended' ? 'Check whitelisted projects' : 'Take me to the form'}
-						</Button>
-					</Link>
+					<div className={'absolute inset-0 top-20 flex h-full w-full justify-center'}>
+						<HeroAsLottie id={'tokens'} />
+					</div>
 				</motion.div>
 			</div>
-
-			<motion.div
-				className={'relative col-span-12 hidden h-[100%] md:col-span-6 md:flex'}
-				variants={customVariants(0.06)}
-				custom={variant}
-				initial={'initial'}
-				animate={'move'}
-				exit={'exit'}>
-				<div className={'absolute inset-0 top-20 flex h-full w-full justify-center'}>
-					<HeroAsLottie id={'tokens'} />
-				</div>
-			</motion.div>
 		</section>
 	);
 }
