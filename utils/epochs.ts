1e11b315b825a8b27d59dbfe44fd05c07d3f4e7b
Majorfi <major-tom.eth@pm.me>
2023-09-14 16:38:26 +0200

    feat: add epoch info

--- utils/epochs.ts
+++ utils/epochs.ts
@@ -26,8 +26,45 @@ export function getCurrentEpoch(): TEpoch {
 }
 
 
+/** 🔵 - Yearn *************************************************************************************
+** To add a new epoch, follow these steps:
+**
+** 1. Copy the first allEpochs.push block.
+** 2. Replace the index with the new epoch number.
+** 3. Replace the list of candidates with the new candidates for this epoch.
+** 4. Replace the list of participants with the new participants for this epoch.
+** 5. Update the ids to the correct voteid.
+**
+** Note:
+** - Both the candidates and participants lists are order sensitive. The first element in the list
+**   will be the choice number 2 in snapshot, as the first one will always be to keep the same as before.
+** - The candidates and participants must respect a specific type that is available in the @tokens.ts file.
+**
+** Here is an example of how to add a new epoch:
+**
+** allEpochs.push({
+** 	index: NEW_EPOCH_NUMBER,
+** 	inclusion: {
+** 		id: 'NEW_VOTE_ID',
+** 		candidates: [
+** 			{...NEW_CANDIDATE_1, index: 0},
+** 			{...NEW_CANDIDATE_2, index: 1},
+** 			{...NEW_CANDIDATE_3, index: 2}
+** 		]
+** 	},
+** 	weight: {
+** 		id: 'NEW_VOTE_ID',
+** 		participants: [
+** 			{...NEW_PARTICIPANT_1, index: 0},
+** 			{...NEW_PARTICIPANT_2, index: 1},
+** 			{...NEW_PARTICIPANT_3, index: 2},
+** 			{...NEW_PARTICIPANT_4, index: 3},
+** 			{...NEW_PARTICIPANT_5, index: 4}
+** 		]
+** 	}
+** });
+**************************************************************************************************/
 const allEpochs: TEpoch[] = [];
-
 // Epoch 0
 allEpochs.push({
 	index: 0,
