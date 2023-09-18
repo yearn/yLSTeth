0b09f4a2194d7a216212649d1139929a4809bce2
Majorfi <major-tom.eth@pm.me>
2023-09-18 13:08:48 +0200

    fix: index

--- utils/constants.ts
+++ utils/constants.ts
@@ -3,20 +3,20 @@ import {zeroAddress} from 'viem';
 
 import {CBETH_TOKEN, SFRXETH_TOKEN, STADERETH_TOKEN, SWETH_TOKEN, WSTETH_TOKEN} from './tokens';
 
-import type {TTokenInfo} from 'contexts/useTokenList';
 import type {TIndexedTokenInfo} from './types';
 
 export const INITIAL_PERIOD_TIMESTAMP = 1_694_044_800;
 export const EPOCH_DURATION = 2_419_200; // 4 weeks
 export const VOTE_START_DELAY = 1_814_400; // 3 weeks
 
-export const NO_CHANGE_LST_LIKE: TTokenInfo = ({
+export const NO_CHANGE_LST_LIKE: TIndexedTokenInfo = ({
 	address: zeroAddress,
 	chainId: 1,
 	decimals: 18,
 	logoURI: '/iconNoChange.svg',
 	name: 'Do Nothing / No Change',
-	symbol: 'Do Nothing / No Change'
+	symbol: 'Do Nothing / No Change',
+	index: -1
 });
 
 export const LST: TIndexedTokenInfo[] = [
