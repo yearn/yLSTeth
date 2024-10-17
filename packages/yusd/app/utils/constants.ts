98ff9084a381060e0e13b62d63b64faf01770f5e
Majorfi <major@ycorpo.com>
2024-10-17 11:54:51 +0200

    fix: upd tokens

--- packages/yusd/app/utils/constants.ts
+++ packages/yusd/app/utils/constants.ts
@@ -27,28 +27,46 @@ export const NO_CHANGE_LST_LIKE: TIndexedTokenInfo = {
 };
 
 export const possibleTokenAddressesToUse = [
-	'0xdC035D45d973E3EC169d2276DDab16f1e407384F',
-	'0x4c9EDD5852cd905f086C759E8383e09bff1E68B3',
-	'0x853d955aCEf822Db058eb8505911ED77F175b99e',
-	'0xdAC17F958D2ee523a2206206994597C13D831ec7',
-	'0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
-	'0x6B175474E89094C44Da98b954EedeAC495271d0F',
-	'0x83F20F44975D03b1b09e64809B757c47f942BEeA',
-	'0xa3931d71877C0E7a3148CB7Eb4463524FEc27fbD', //
-	'0x9D39A5DE30e57443BfF2A8307A4256c8797A3497', //
-	'0xA663B02CF0a4b149d2aD41910CB81e23e1c41c32', //
-	'0x96F6eF951840721AdBF46Ac996b59E0235CB985C' //
+	'0xdC035D45d973E3EC169d2276DDab16f1e407384F', // USDS
+	'0x4c9EDD5852cd905f086C759E8383e09bff1E68B3', // USDe
+	'0x853d955aCEf822Db058eb8505911ED77F175b99e', // FRAX
+	'0xdAC17F958D2ee523a2206206994597C13D831ec7', // USDT
+	'0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', // USDC
+	'0x6B175474E89094C44Da98b954EedeAC495271d0F', // DAI
+	'0x83F20F44975D03b1b09e64809B757c47f942BEeA', // sDAI
+	'0xa3931d71877C0E7a3148CB7Eb4463524FEc27fbD', // sUSDS
+	'0x9D39A5DE30e57443BfF2A8307A4256c8797A3497', // sUSDe
+	'0xA663B02CF0a4b149d2aD41910CB81e23e1c41c32', // sFRAX
+	'0x96F6eF951840721AdBF46Ac996b59E0235CB985C' // USDY
 ];
 
 export const possibleTokensToVoteFor: TDict<TIndexedTokenInfo> = {
+	'0xa3931d71877C0E7a3148CB7Eb4463524FEc27fbD': {
+		address: '0xa3931d71877C0E7a3148CB7Eb4463524FEc27fbD',
+		index: 2,
+		balance: zeroNormalizedBN,
+		chainID: 1,
+		decimals: 18,
+		logoURI: 'https://assets.smold.app/api/token/1/0xa3931d71877C0E7a3148CB7Eb4463524FEc27fbD/logo-128.png',
+		name: 'Staked USDS',
+		symbol: 'sUSDS',
+		value: 0
+	},
+	'0x9D39A5DE30e57443BfF2A8307A4256c8797A3497': {
+		address: '0x9D39A5DE30e57443BfF2A8307A4256c8797A3497',
+		index: 2,
+		balance: zeroNormalizedBN,
+		chainID: 1,
+		decimals: 18,
+		logoURI: 'https://assets.smold.app/api/token/1/0x9D39A5DE30e57443BfF2A8307A4256c8797A3497/logo-128.png',
+		name: 'Staked USDe',
+		symbol: 'sUSDe',
+		value: 0
+	},
 	'0xA663B02CF0a4b149d2aD41910CB81e23e1c41c32': {
 		address: '0xA663B02CF0a4b149d2aD41910CB81e23e1c41c32',
 		index: 1,
-		balance: {
-			raw: 0n,
-			normalized: 0,
-			display: '0'
-		},
+		balance: zeroNormalizedBN,
 		chainID: 1,
 		decimals: 18,
 		logoURI: 'https://assets.coingecko.com/coins/images/35383/large/sfrax.png?1708445569',
@@ -56,19 +74,15 @@ export const possibleTokensToVoteFor: TDict<TIndexedTokenInfo> = {
 		symbol: 'sFRAX',
 		value: 0
 	},
-	'0x9D39A5DE30e57443BfF2A8307A4256c8797A3497': {
-		address: '0x9D39A5DE30e57443BfF2A8307A4256c8797A3497',
-		index: 2,
-		balance: {
-			raw: 0n,
-			normalized: 0,
-			display: '0'
-		},
+	'0x96F6eF951840721AdBF46Ac996b59E0235CB985C': {
+		address: '0x96F6eF951840721AdBF46Ac996b59E0235CB985C',
+		index: 1,
+		balance: zeroNormalizedBN,
 		chainID: 1,
 		decimals: 18,
-		logoURI: 'https://assets.smold.app/api/token/1/0x9D39A5DE30e57443BfF2A8307A4256c8797A3497/logo-128.png',
-		name: 'Staked USDe',
-		symbol: 'sUSDe',
+		logoURI: 'https://assets.coingecko.com/coins/images/31700/large/usdy_%281%29.png?1696530524',
+		name: 'Ondo U.S. Dollar Yield',
+		symbol: 'USDY',
 		value: 0
 	}
 };
@@ -76,11 +90,7 @@ export const possibleTokensToVoteFor: TDict<TIndexedTokenInfo> = {
 export const whitelistedLST: TDict<TIndexedTokenInfo> = {
 	'0x83F20F44975D03b1b09e64809B757c47f942BEeA': {
 		address: '0x83F20F44975D03b1b09e64809B757c47f942BEeA',
-		balance: {
-			raw: 0n,
-			normalized: 0,
-			display: '0'
-		},
+		balance: zeroNormalizedBN,
 		chainID: 1,
 		decimals: 18,
 		logoURI: 'https://assets.smold.app/api/token/1/0x83F20F44975D03b1b09e64809B757c47f942BEeA/logo-128.png',
@@ -91,11 +101,7 @@ export const whitelistedLST: TDict<TIndexedTokenInfo> = {
 	},
 	'0x3d2FdAb1fA27ddDe9dcB77f151768beB839bC9ED': {
 		address: '0x3d2FdAb1fA27ddDe9dcB77f151768beB839bC9ED',
-		balance: {
-			raw: 0n,
-			normalized: 0,
-			display: '0'
-		},
+		balance: zeroNormalizedBN,
 		chainID: 1,
 		decimals: 6,
 		logoURI: 'https://assets.smold.app/api/token/1/0xdAC17F958D2ee523a2206206994597C13D831ec7/logo-128.png',
